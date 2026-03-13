6e638ef010c88657c7974d1a8e248befd8969371
Konstantin Doloz <takeonfaith6@gmail.com>
2026-03-14 00:10:40 +0300

    first commit

---
+++ SwiftUI-Weather/WeatherButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherButtonView.swift
+//  SwiftUI-Weather
+//
+//  Created by Konstantin Doloz on 25.10.2023.
+//
+
+import SwiftUI
+
+struct WeatherButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherButtonView()
+    }
+}
